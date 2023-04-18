34e8a785d4bf1190b0919b42efca1eb5a723ba29
Louwlife <lourensvn3@gmail.com>
2023-04-18 09:31:52 +0200

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir -v ~/.user-temp
+TEMP=~/.user-temp pip install --user qiskit[visualization] qiskit-machine-learning
+git clone https://gitlab-ce.gwdg.de/lourens.van-niekerk/isc-2023-qml-tutorial.git
+
